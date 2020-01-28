a8f602d0fb65096a8550b472ca2e1a3a8b631bd9
wizage <wizagesmax@gmail.com>
2020-01-28 10:38:01 -0800

    Fixed lint

--- provider-utils/awscloudformation/cloudformation-templates/vod-helpers/LambdaFunctions/OutputLambda/index.js
+++ provider-utils/awscloudformation/cloudformation-templates/vod-helpers/LambdaFunctions/OutputLambda/index.js
@@ -7,7 +7,7 @@ const s3 = new AWS.S3({});
 exports.handler = function (event, context) {
 /* eslint-enable */
 
-/*
+  /*
 Function that triggers on the output bucket.
 
 event.Records contains an array of S3 records that you can take action on.
