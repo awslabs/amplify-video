1ed0a16b7d4d4f763164833885925aa6e1c797cb
Nathan AGEZ <nathan.agez@epitech.eu>
2021-02-05 10:18:51 +0100

    fix(paths): Refactor scripts exec path

--- scripts/setup.js
+++ scripts/setup.js
@@ -2,7 +2,7 @@ const { exec } = require('../provider-utils/awscloudformation/utils/headless-mod
 
 module.exports = async function setup() {
   console.log('\namplify init');
-  await exec('bash', ['./scripts/headless_scripts/init-new-project.sh']);
+  await exec('bash', ['./scripts/headless/init-new-project.sh']);
   console.log('\namplify add video');
-  await exec('bash', ['./scripts/headless_scripts/add-ivs.sh']);
+  await exec('bash', ['./scripts/headless/add-ivs.sh']);
 };
