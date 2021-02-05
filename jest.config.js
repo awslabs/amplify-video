02add326b7e64d361b03f62b3cbfdd67b4abfadf
Nathan AGEZ <nathan.agez@epitech.eu>
2021-02-05 15:22:11 +0100

    refactor(jest): Add dev-test mode

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   verbose: true,
   globalSetup: './scripts/setup.js',
-  globalTeardown: './scripts/teardown.js',
+  globalTeardown: process.env.NODE_ENV === 'test' ? './scripts/teardown.js' : null, // Jest automatically set NODE_ENV to test if it's not already set to something else.
 };
