eb562738835b2a172f631f4d561416d9d2ab8569
Nathan AGEZ <nathan.agez@epitech.eu>
2021-02-05 15:22:11 +0100

    refactor(jest): Add dev-test mode

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 const { exec } = require('../provider-utils/awscloudformation/utils/headless-mode');
 
 module.exports = async function setup() {
-  console.log('\namplify init');
-  await exec('bash', ['./scripts/headless_scripts/init-new-project.sh']);
-  console.log('\namplify add video');
-  await exec('bash', ['./scripts/headless_scripts/add-ivs.sh']);
+  if (process.env.NODE_ENV !== 'test') {
+    const directoryPath = path.join(__dirname, `../${process.env.AMP_PATH}/amplify`);
+    if (!fs.existsSync(directoryPath)) {
+      throw new Error(`No amplify project found, make sure to set AMP_PATH with correct path.\nActual path: ${directoryPath}`);
+    }
+  } else {
+    await executeScripts();
+  }
 };
+
+async function executeScripts() {
+  console.log('\namplify init');
+  await exec('bash', ['./scripts/headless/init-new-project.sh']);
+  console.log('\namplify add video');
+  await exec('bash', ['./scripts/headless/add-ivs.sh']);
+}
