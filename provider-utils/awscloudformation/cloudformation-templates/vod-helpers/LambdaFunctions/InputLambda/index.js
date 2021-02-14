ca3caebcffc79220f84ce7dbeba83ce236a08607
Corneliu <cornel.croitoru@gmail.com>
2021-02-14 13:42:50 +0100

    add support for multiple encoding formats

--- provider-utils/awscloudformation/cloudformation-templates/vod-helpers/LambdaFunctions/InputLambda/index.js
+++ provider-utils/awscloudformation/cloudformation-templates/vod-helpers/LambdaFunctions/InputLambda/index.js
@@ -3,25 +3,12 @@
 /* eslint-disable */
 const AWS = require('aws-sdk');
 /* eslint-enable */
-const jobSettings = require('./settings.json');
 // Set the region
-
-exports.handler = async (event) => {
-  AWS.config.update({ region: event.awsRegion });
-  console.log(event);
-  if (event.Records[0].eventName.includes('ObjectCreated')) {
-    await createJob(event.Records[0].s3);
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(`Transcoding your file: ${event.Records[0].s3.object.key}`),
-    };
-    return response;
-  }
-};
-
 // Function to submit job to Elemental MediaConvert
 async function createJob(eventObject) {
   let mcClient = new AWS.MediaConvert();
+  const allOutputs = [];
+
   if (!AWS.config.mediaconvert) {
     try {
       const endpoints = await mcClient.describeEndpoints().promise();
@@ -42,10 +29,55 @@ async function createJob(eventObject) {
   const FileName = AddedKey.split('.').slice(0, -1).join('.');
   const Bucket = eventObject.bucket.name;
   const outputBucketName = process.env.OUTPUT_BUCKET;
+  const tmplName = process.env.ARN_TEMPLATE.split(':')[5].split('/')[1];
+  const tmpl = await mcClient.getJobTemplate({ Name: tmplName }).promise();
 
-  // Set the output to have the filename (without extension) as a folder
-  jobSettings.OutputGroups[0].OutputGroupSettings.HlsGroupSettings.Destination = `s3://${outputBucketName}/${FileName}/`;
-  jobSettings.Inputs[0].FileInput = `s3://${Bucket}/${decodeURIComponent(AddedKey.replace(/\+/g, ' '))}`;
+  tmpl.JobTemplate.Settings.OutputGroups.forEach(group => {
+    if (group.OutputGroupSettings.Type === 'HLS_GROUP_SETTINGS') {
+      group.OutputGroupSettings.HlsGroupSettings.Destination = `s3://${outputBucketName}/${FileName}/`;
+      allOutputs.push({
+        Name: 'Apple HLS',
+        Outputs: [],
+        OutputGroupSettings: group.OutputGroupSettings,
+      });
+    }
+
+    if (group.OutputGroupSettings.Type === 'DASH_ISO_GROUP_SETTINGS') {
+      group.OutputGroupSettings.DashIsoGroupSettings.Destination = `s3://${outputBucketName}/${FileName}/`;
+      allOutputs.push({
+        Name: 'DASH ISO',
+        Outputs: [],
+        OutputGroupSettings: group.OutputGroupSettings,
+      });
+    }
+
+    if (group.OutputGroupSettings.Type === 'FILE_GROUP_SETTINGS') {
+      group.OutputGroupSettings.DashIsoGroupSettings.Destination = `s3://${outputBucketName}/${FileName}/`;
+      allOutputs.push({
+        Name: 'File Group',
+        Outputs: [],
+        OutputGroupSettings: group.OutputGroupSettings,
+      });
+    }
+
+    if (group.OutputGroupSettings.Type === 'MS_SMOOTH_GROUP_SETTINGS') {
+      group.OutputGroupSettings.DashIsoGroupSettings.Destination = `s3://${outputBucketName}/${FileName}/`;
+      allOutputs.push({
+        Name: 'MS Smooth',
+        Outputs: [],
+        OutputGroupSettings: group.OutputGroupSettings,
+      });
+    }
+
+    if (group.OutputGroupSettings.Type === 'CMAF_GROUP_SETTINGS') {
+      group.OutputGroupSettings.DashIsoGroupSettings.Destination = `s3://${outputBucketName}/${FileName}/`;
+      allOutputs.push({
+        Name: 'CMAF',
+        Outputs: [],
+        OutputGroupSettings: group.OutputGroupSettings,
+      });
+    }
+  });
 
   let queueARN = '';
   if (process.env.QUEUE_ARN) {
@@ -57,13 +89,52 @@ async function createJob(eventObject) {
     }).promise();
     queueARN = q.Queue.Arn;
   }
+  const allGroups = {
+    OutputGroups: allOutputs,
+    AdAvailOffset: 0,
+    Inputs: [
+      {
+        AudioSelectors: {
+          'Audio Selector 1': {
+            Offset: 0,
+            DefaultSelection: 'DEFAULT',
+            ProgramSelection: 1,
+          },
+        },
+        VideoSelector: {
+          ColorSpace: 'FOLLOW',
+        },
+        FilterEnable: 'AUTO',
+        PsiControl: 'USE_PSI',
+        FilterStrength: 0,
+        DeblockFilter: 'DISABLED',
+        DenoiseFilter: 'DISABLED',
+        TimecodeSource: 'ZEROBASED',
+        FileInput: `s3://${Bucket}/${decodeURIComponent(AddedKey.replace(/\+/g, ' '))}`,
+      },
+    ],
+  };
 
   const jobParams = {
     JobTemplate: process.env.ARN_TEMPLATE,
     Queue: queueARN,
     UserMetadata: {},
     Role: process.env.MC_ROLE,
-    Settings: jobSettings,
+    Settings: allGroups,
   };
   await mcClient.createJob(jobParams).promise();
 }
+
+//exports.handler = async (event) => {
+//exports.handler =  async function(event) {
+  exports.handler = async function(event) {
+  AWS.config.update({ region: event.awsRegion });
+  if (event.Records[0].eventName.includes('ObjectCreated')) {
+    await createJob(event.Records[0].s3);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(`Transcoding your file: ${event.Records[0].s3.object.key}`),
+    };
+    return response;
+  }
+};
