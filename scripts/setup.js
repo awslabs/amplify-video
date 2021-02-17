28fc1b60e128cd23eb3a032de1a2218cb4185ec9
Nathan AGEZ <nathan.agez@epitech.eu>
2021-02-17 17:03:58 +0100

    refactor(setup): Delete amplify project on error during setup

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,8 +1,13 @@
 const { exec } = require('../provider-utils/awscloudformation/utils/headless-mode');
 
 module.exports = async function setup() {
-  console.log('\namplify init');
-  await exec('bash', ['./scripts/headless/init-new-project.sh']);
-  console.log('\namplify add video');
-  await exec('bash', ['./scripts/headless/add-ivs.sh']);
+  try {
+    console.log('\namplify init');
+    await exec('bash', ['./scripts/headless/init-new-project.sh']);
+    console.log('\namplify add video');
+    await exec('bash', ['./scripts/headless/add-ivs.sh']);
+  } catch (error) {
+    await exec('bash', ['./scripts/headless/amplify-delete.sh']);
+    throw (new Error(error));
+  }
 };
