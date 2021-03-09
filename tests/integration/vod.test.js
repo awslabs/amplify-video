73b01d43740874dbe1fe85a23a65c70bb6cd68e1
SPANIER Nathan <spaniernathan67@gmail.com>
2021-03-09 11:22:42 +0100

    tests(CFDistribution): added tests for VOD

--- tests/integration/vod.test.js
+++ tests/integration/vod.test.js
@@ -5,7 +5,9 @@ const axios = require('axios');
 const request = require('supertest');
 const CloudFrontTokenGen = require('../../provider-utils/awscloudformation/cloudformation-templates/vod-helpers/LambdaFunctions/CloudFrontTokenGen/index.js');
 
-describe('CloudFront signed urls', () => {
+// AWS.config.update({ region: 'us-west-2' });
+
+describe('VOD Tests', () => {
   let token;
   let domainName;
   let projectProps;
@@ -59,56 +61,100 @@ describe('CloudFront signed urls', () => {
     }).promise()));
   });
 
+  describe('CloudFront signed urls', () => {
 
-  test('Should generate Cloudfront token', async () => {
-    if (projectNames.length === 0) {
-      console.log('No VoD projects found, passing to next tests');
-      return;
-    }
-    const cloudfront = new AWS.CloudFront();
-
-    const enabledCDNProjects = projectProps
-      .filter(props => props.contentDeliveryNetwork.enableDistribution)
-      .map(props => props);
-
-    await Promise.all(enabledCDNProjects.map(async (project) => {
-      const publicKeys = await cloudfront.listPublicKeys().promise();
-      const publicKey = publicKeys.PublicKeyList.Items
-        .filter(key => key.Name === project.contentDeliveryNetwork.publicKeyName)
-        .map(key => key)[0];
-      const keyGroups = await cloudfront.listKeyGroups().promise();
-      const keyGroup = keyGroups.KeyGroupList.Items
-        .filter(item => item.KeyGroup.KeyGroupConfig.Name === project.contentDeliveryNetwork.functionName.replace('tokenGen', 'KeyGroup'))
-        .map(group => group)[0];
-      const cloudFrontDistributions = await cloudfront.listDistributionsByKeyGroup({
-        KeyGroupId: keyGroup.KeyGroup.Id,
-      }).promise();
-      const distributionId = cloudFrontDistributions.DistributionIdList.Items[0];
-      const cloudFrontDistribution = await cloudfront.getDistribution({
-        Id: distributionId,
-      }).promise();
-      domainName = cloudFrontDistribution.Distribution.DomainName;
-      process.env.SecretPem = project.contentDeliveryNetwork.secretPem;
-      process.env.PemID = publicKey.Id;
-      process.env.Host = domainName;
-      token = await CloudFrontTokenGen.signPath('test');
-      expect(token).not.toBe(undefined);
-    }));
+    test('Should generate Cloudfront token', async () => {
+      if (projectNames.length === 0) {
+        console.log('No VoD projects found, passing to next tests');
+        return;
+      }
+      const cloudfront = new AWS.CloudFront();
+  
+      const enabledCDNProjects = projectProps
+        .filter(props => props.contentDeliveryNetwork.enableDistribution)
+        .map(props => props);
+  
+      await Promise.all(enabledCDNProjects.map(async (project) => {
+        const publicKeys = await cloudfront.listPublicKeys().promise();
+        const publicKey = publicKeys.PublicKeyList.Items
+          .filter(key => key.Name === project.contentDeliveryNetwork.publicKeyName)
+          .map(key => key)[0];
+        const keyGroups = await cloudfront.listKeyGroups().promise();
+        const keyGroup = keyGroups.KeyGroupList.Items
+          .filter(item => item.KeyGroup.KeyGroupConfig.Name === project.contentDeliveryNetwork.functionName.replace('tokenGen', 'KeyGroup'))
+          .map(group => group)[0];
+        const cloudFrontDistributions = await cloudfront.listDistributionsByKeyGroup({
+          KeyGroupId: keyGroup.KeyGroup.Id,
+        }).promise();
+        const distributionId = cloudFrontDistributions.DistributionIdList.Items[0];
+        const cloudFrontDistribution = await cloudfront.getDistribution({
+          Id: distributionId,
+        }).promise();
+        domainName = cloudFrontDistribution.Distribution.DomainName;
+        process.env.SecretPem = project.contentDeliveryNetwork.secretPem;
+        process.env.PemID = publicKey.Id;
+        process.env.Host = domainName;
+        token = await CloudFrontTokenGen.signPath('test');
+        expect(token).not.toBe(undefined);
+      }));
+    });
+  
+    test('Should return 200 HTTP status code', async () => {
+      if (projectNames.length === 0) {
+        console.log('No VoD projects found, passing to next tests');
+        return;
+      }
+      await request(`https://${domainName}`).get(`/test/test.png${token}`).expect(200);
+    });
+  
+    test('Should return 403 HTTP status code', async () => {
+      if (projectNames.length === 0) {
+        console.log('No VoD projects found, passing to next tests');
+        return;
+      }
+      await request(`https://${domainName}`).get('/test/test.png').expect(403);
+    });
   });
 
-  test('Should return 200 HTTP status code', async () => {
-    if (projectNames.length === 0) {
-      console.log('No VoD projects found, passing to next tests');
-      return;
-    }
-    await request(`https://${domainName}`).get(`/test/test.png${token}`).expect(200);
-  });
+  describe('CloudFront distribution', () => {
 
-  test('Should return 403 HTTP status code', async () => {
-    if (projectNames.length === 0) {
-      console.log('No VoD projects found, passing to next tests');
-      return;
-    }
-    await request(`https://${domainName}`).get('/test/test.png').expect(403);
-  });
-});
+    test('GET, HEAD, OPTIONS Should return 200 status code', async () => {
+      if (projectNames.length === 0) {
+        console.log('No VoD projects found, passing to next tests');
+        return;
+      }
+      await request(`https://${domainName}`).get(`/test/test.png${token}`).expect(200);
+      await request(`https://${domainName}`).head(`/test/test.png${token}`).expect(200);
+      // await request(`https://${domainName}`).options(`/test/test.png${token}`).expect(200);
+    });
+  
+    test('POST, PUT, PATCH, DELETE Should return 403 status code', async () => {
+      if (projectNames.length === 0) {
+        console.log('No VoD projects found, passing to next tests');
+        return;
+      }
+      await request(`https://${domainName}`).post(`/test/test.png${token}`).expect(403);
+      await request(`https://${domainName}`).put(`/test/test.png${token}`).expect(403);
+      await request(`https://${domainName}`).patch(`/test/test.png${token}`).expect(403);
+      await request(`https://${domainName}`).delete(`/test/test.png${token}`).expect(403);
+    });
+
+    /*
+    test('Access-Control-Request-Method Should be set', async () => {
+      if (projectNames.length === 0) {
+        console.log('No VoD projects found, passing to next tests');
+        return;
+      }
+      await request(`https://${domainName}`)
+      .get(`/test/test.png${token}`)
+      .set({
+        'Access-Control-Request-Method': 'GET, HEAD, OPTIONS',
+        'Access-Control-Request-Headers': 'Content-Type'
+      })
+      .then(res => {
+        console.log(res.header)
+      });
+    });
+    */
+  })
+})
