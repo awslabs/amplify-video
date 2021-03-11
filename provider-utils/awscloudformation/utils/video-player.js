2909706507418ef47cd09795efbea975bab268ee
Nathan AGEZ <nathan.agez@epitech.eu>
2021-03-11 17:33:13 +0100

    feat(video-player): Add player template

--- provider-utils/awscloudformation/utils/video-player.js
+++ provider-utils/awscloudformation/utils/video-player.js
@@ -1,4 +1,6 @@
 const chalk = require('chalk');
+const ejs = require('ejs');
+const fs = require('fs');
 
 module.exports = {
   setupPlayer,
@@ -8,8 +10,22 @@ async function setupPlayer(context, resourceName) {
   const { amplify } = context;
   const amplifyMeta = amplify.getProjectMeta();
   if ('output' in amplifyMeta.video[resourceName]) {
-    // TODO: Remove it
-    console.log('Works');
+    const projectConfigFilePath = context.amplify.pathManager.getProjectConfigFilePath();
+    let projectConfig = await fs.readFileSync(projectConfigFilePath, { encoding: 'utf-8' });
+    projectConfig = JSON.parse(projectConfig);
+
+    if (projectConfig[projectConfig.frontend].framework === 'react') {
+      const props = {
+        ivs: true,
+      };
+      const videoTemplate = await fs.readFileSync(`${__dirname}/../player-templates/video-player.jsx.ejs`, { encoding: 'utf-8' });
+      const appendVideoTemplate = ejs.render(videoTemplate, props);
+
+      await fs.writeFileSync(`${context.amplify.pathManager.searchProjectRootPath()}/${projectConfig[projectConfig.frontend].config.SourceDir}/VideoPlayer.jsx`, appendVideoTemplate);
+      context.print.success('\nConfiguration complete.');
+    } else {
+      context.print.warning(chalk`{bold ${projectConfig[projectConfig.frontend].framework} is not supported for the moment.}`);
+    }
   } else {
     context.print.warning(chalk`{bold You have not pushed ${resourceName} to the cloud yet.}`);
   }
