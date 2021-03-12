10564ecd15550902a23543ef28295acee24b2471
Nathan AGEZ <nathan.agez@epitech.eu>
2021-03-12 15:26:34 +0100

    feat(video-player): Add React video player

--- provider-utils/awscloudformation/utils/video-player.js
+++ provider-utils/awscloudformation/utils/video-player.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk');
 const ejs = require('ejs');
 const fs = require('fs');
+const { exec } = require('./headless-mode');
 
 module.exports = {
   setupPlayer,
@@ -11,18 +12,29 @@ async function setupPlayer(context, resourceName) {
   const amplifyMeta = amplify.getProjectMeta();
   if ('output' in amplifyMeta.video[resourceName]) {
     const projectConfigFilePath = context.amplify.pathManager.getProjectConfigFilePath();
+    const { serviceType, output } = amplifyMeta.video[resourceName];
     let projectConfig = await fs.readFileSync(projectConfigFilePath, { encoding: 'utf-8' });
     projectConfig = JSON.parse(projectConfig);
 
     if (projectConfig[projectConfig.frontend].framework === 'react') {
       const props = {
-        ivs: true,
+        ivs: serviceType !== 'ivs',
       };
       const videoTemplate = await fs.readFileSync(`${__dirname}/../player-templates/video-player.jsx.ejs`, { encoding: 'utf-8' });
       const appendVideoTemplate = ejs.render(videoTemplate, props);
 
+      const videoComponentTemplate = await fs.readFileSync(`${__dirname}/../player-templates/video-component.ejs`, { encoding: 'utf-8' });
+      const appendVideoComponentTemplate = ejs.render(videoComponentTemplate, { src: output.oVideoOutput });
+
       await fs.writeFileSync(`${context.amplify.pathManager.searchProjectRootPath()}/${projectConfig[projectConfig.frontend].config.SourceDir}/VideoPlayer.jsx`, appendVideoTemplate);
-      context.print.success('\nConfiguration complete.');
+      context.print.info(chalk.bold('Installing video.js package.'));
+      await exec('npm', ['install', 'video.js']);
+      if (serviceType.ivs) {
+        await exec('npm', ['install', 'amazon-ivs-player']);
+      }
+      context.print.success('Configuration complete.');
+      context.print.success(`Copy the following ${projectConfig[projectConfig.frontend].framework} component:\n`);
+      context.print.info(appendVideoComponentTemplate);
     } else {
       context.print.warning(chalk`{bold ${projectConfig[projectConfig.frontend].framework} is not supported for the moment.}`);
     }
