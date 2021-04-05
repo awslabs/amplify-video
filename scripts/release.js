c40488ca4f6878ff553bc48ea93d68fab596e59c
Sam Patzer <wizagesmax@gmail.com>
2021-04-05 10:19:13 -0700

    temp removing release.js

--- scripts/release.js
+++ scripts/release.js
@@ -1,25 +1,25 @@
-const inquirer = require('inquirer');
+// const inquirer = require('inquirer');
 
-const versionQuestion = [
-  {
-    name: 'version',
-    message: 'Define this version upgrade (x.y.z)',
-    type: 'list',
-    choices:[{
-      "name": "Major (x)",
-      "value":"major"
-    },
-    {
-      "name":"Minor (y)",
-      "value":"minor"
-    },
-    {
-      "name":"Patch (z)",
-      "value":"patch"
-    }],
-    default: 'patch',
-  }];
+// const versionQuestion = [
+//   {
+//     name: 'version',
+//     message: 'Define this version upgrade (x.y.z)',
+//     type: 'list',
+//     choices:[{
+//       "name": "Major (x)",
+//       "value":"major"
+//     },
+//     {
+//       "name":"Minor (y)",
+//       "value":"minor"
+//     },
+//     {
+//       "name":"Patch (z)",
+//       "value":"patch"
+//     }],
+//     default: 'patch',
+//   }];
 
-inquirer.prompt(versionQuestion).then(answers => {
+// inquirer.prompt(versionQuestion).then(answers => {
   
-}).catch(e => console.log(e));
+// }).catch(e => console.log(e));
