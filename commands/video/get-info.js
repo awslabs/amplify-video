1893f31a51926f5b5f7ad0fdbcd79693c4484797
Conor Woods <conorw@gmail.com>
2021-08-17 09:47:38 +0100

    Headless get_info command for CICD

--- commands/video/get-info.js
+++ commands/video/get-info.js
@@ -23,7 +23,13 @@ module.exports = {
         default: Object.keys(amplifyMeta[category])[0],
       },
     ];
-    const props = await inquirer.prompt(chooseProject);
+    
+    let props;
+    if (context.parameters.options.default) {
+      props = {resourceName:chooseProject[0].default};
+    } else{
+      props = await inquirer.prompt(chooseProject);
+    }
 
     const options = amplifyMeta.video[props.resourceName];
 
